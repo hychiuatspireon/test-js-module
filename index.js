3018c8f47f762ce1f086d7cf10943d87abbaecb1
Hao Yun Chiu <hychiu@spireon.com>
2018-10-21 17:07:49 -0700

    trying different exports

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports = function(width, height) {
-       	return width * height;
-};
+// module.exports = function(width, height) {
+//        	return width * height;
+// };
+
+function getListObjects(listObjs) {
+  console.log(listObjs); 
+}
+
+module.exports = {
+	getListObjects;
+}
+
