50ca742c5c083e713e54560eeb9fa78f9cba17f8
Hao Yun Chiu <hychiu@spireon.com>
2018-10-21 17:33:04 -0700

    added steps to add addl attribute to JSON object

--- index.js
+++ index.js
@@ -4,6 +4,21 @@
 
 function getListObjects(listObjs) {
   console.log(listObjs); 
+
+  for (var i = 0; i < listObjs.length ; i++) {
+  	addNewDateToObject(listObjs[i]);
+  }
+
+  console.log(listObjs);
+}
+
+function addNewDateToObject(obj) {
+	var date = new Date();
+	var newDate = new Date(date.getTime() - 36000000); // minus 10 hours from current time
+	var regex = /\.\d{3}/;
+	var newDateStr = newDate.toISOString().replace(regex, ""); // remove .xxx in the timestamp string
+
+	obj.newDate = newDateStr;
 }
 
 module.exports = {
